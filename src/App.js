6748c0af7cc044469d96f8a5862c4e0850e39a04
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-03-10 14:06:56 -0600

    Fetch data from github API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    username: "leroycep",
+    info: {},
+    followers: []
+  };
+
+  componentDidMount() {
+    // Get user information
+    axios
+      .get(`https://api.github.com/users/${this.state.username}`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ info: res.data });
+      })
+      .catch(e => console.log("Failed to retrieve user information", e));
+
+    // Get user's followers
+    axios
+      .get(`https://api.github.com/users/${this.state.username}/followers`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ followers: res.data });
+      })
+      .catch(e => console.log("Failed to retrieve user information", e));
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>{this.state.info.name}</h3>
+        <div>{JSON.stringify(this.state.info)}</div>
+        <div>{JSON.stringify(this.state.followers)}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
